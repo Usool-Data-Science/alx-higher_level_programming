045cb27f5bb7043b4bc65f8b99ec39cea582e47b
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-12-03 15:44:46 +0100

    Update

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * is_palindrome - A function to check if head is palindrome
+  * @head: The entry point of the list.
+  *
+  * Return: 0 if not and 1 if its a palindrom
+  */
+
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return (1);
+	else
+		return (0);
+}
