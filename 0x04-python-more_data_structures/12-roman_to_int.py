3a3315cf39b763ab0f93bedb9588d10ed247ad3a
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-12-07 00:52:32 +0100

    Solve task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,13 +10,15 @@ def roman_to_int(roman_string):
             'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5,
             'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9, 'X': 10,
             'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
+    result, previous_result = 0, 0
     if not roman_string or not isinstance(roman_string, str):
         return (result)
 
-    if roman_string in list(roman_dict):
-        result += roman_dict[roman_string]
-    else:
-        for num in roman_string:
-            result += roman_dict[num]
+    for i in reversed(roman_string):
+        value = roman_dict.get(i, 0)
+        if value < previous_result:
+            result -= value
+        else:
+            result += value
+        previous_result = value
     return (result)
