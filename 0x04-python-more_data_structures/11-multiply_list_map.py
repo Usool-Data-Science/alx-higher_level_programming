97e509482954c0108c76440580764299d5333e76
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-12-07 00:15:25 +0100

    Solve task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-"""
-    A function that multiplies
-    a number to each element of a list
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(map(lambda a: a*number, my_list))
-    return (new_list)
+    return (list(map(lambda a: a*number, my_list)))
