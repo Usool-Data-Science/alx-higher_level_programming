5db55200e422acca99c9a8bce59a11369b43e11e
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-12-18 15:51:51 +0100

    Solve task 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
     result = 0
 
@@ -8,9 +7,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += (a ** b) / i
-        except:
+        except Exception:
             result += b + a
             break
-
     return result
-
