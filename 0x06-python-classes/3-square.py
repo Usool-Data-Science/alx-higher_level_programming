8ed1ca0cbc2ae8b6f4e340d79113f83d95bc2d97
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-12-19 16:38:59 +0100

    Solve task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module creates a blueprint(class) for a circle
+The blueprint assigns size to the circle instances
+"""
+
+
+class Square(object):
+    """A blueprint for squares
+       It has only one argument and it is size.
+    """
+    def __init__(self, size=0):
+        """Instantiate a circle with size
+           Args:
+               size (int): The size of the circle.
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Calculates the area of the square
+            Area = side ** 2
+            Args:
+               Takes no arguments.
+            Return:
+               The Area of the square
+        """
+        return self.__size ** 2
