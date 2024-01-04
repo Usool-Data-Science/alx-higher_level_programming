3ad412e8ae605981a78474e2fc9fa55f4f4b7dd3
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-01-04 12:26:33 +0100

    Solve task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Rectangle(object):
+    """
+    A class for creating an empty rectangle object
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This are more projects on Object Oriented Programming in Python
