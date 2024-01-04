853248eb57026551716b05f31eee47b16f6bbc1a
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-01-04 12:32:33 +0100

    Solve task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+A class module that creates a rectangle class
+"""
+
 class Rectangle(object):
     """
     A class for creating an empty rectangle object
     """
-    pass
+    def __init__(self):
+        """
+        An instantiator function for each instance
+        """
+        pass
+
