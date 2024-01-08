47a006d774352212e918b132e4607ce0f289397f
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2024-01-08 18:04:13 +0100

    Update

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,13 +8,6 @@ class BaseGeometry(object):
     """
     A blueprint for base geometry
     """
-
-    def __init__(self):
-        """
-        Initialize the base geometry
-        """
-        pass
-
     def area(self):
         """
         Just raise NotImplementedError()
