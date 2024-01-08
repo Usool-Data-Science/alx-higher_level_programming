4b3fcbe97ea44a77b513231f10c695b8cb8deed2
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-01-08 17:11:39 +0100

    Solve task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+Check for inheritance
+"""
+
+
+class Square(Rectangle):
+    """
+    A blueprint for a Square
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the rectangle
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __repr__(self):
+        return "[{}] {}/{}".format(
+            type(self).__name__, self.__size, self.__size)
