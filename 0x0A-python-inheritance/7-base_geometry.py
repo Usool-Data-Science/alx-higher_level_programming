fc1c6ee9335b0e6646a1cee9dde3768f8908db14
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-01-08 12:40:56 +0100

    Solve task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Check for inheritance
+"""
+
+
+class BaseGeometry(object):
+    """
+    A blueprint for base geometry
+    """
+
+    def __init__(self):
+        """
+        Initialize the base geometry
+        """
+        pass
+
+    def area(self):
+        """
+        Just raise NotImplementedError()
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        A validator for the value
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        else:
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
