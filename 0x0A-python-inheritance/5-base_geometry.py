606e129252f6f179ca8e92f6dd12caa90d7f763c
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-01-08 11:12:13 +0100

    Solve task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Check for inheritance
+"""
+
+
+class BaseGeometry(object):
+    """
+    A blueprint for base geometry
+    """
+
+    def __init__(self):
+        """
+        Initialize the base geometry
+        """
+        pass
