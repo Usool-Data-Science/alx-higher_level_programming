908f88b3d70f8835bc519ccefaa851c783ed2bbf
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2024-01-10 05:30:31 +0100

    Solve task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A module that prints the dictionary representation of students'
+class
+"""
+
+
+class Student(object):
+    """
+    A Blue-print for student qualities.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        An instantiator for the object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Prints the dictionary representation of Student object
+        """
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for k,v in self.__dict__.items():
+            if k in attrs:
+                my_dict[k] = v
+        return my_dict
+
+    def reload_from_json(self, json):
+        """
+        Loads python object from JSON item.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
