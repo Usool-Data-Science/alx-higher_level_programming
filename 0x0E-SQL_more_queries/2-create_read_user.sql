c96f1da42ffb864b5a74fc6989ea354d09f213bd
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-02-14 23:04:07 +0100

    Solve mandatory tasks

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates hbtn_0d_2 and user_0d_2 (Database and user) respectively
+-- create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user
+CREATE USER IF NOT EXISTS user_0d_02@localhost IDENTIFIED by 'user_0d_2_pwd';
+-- grant SELECT privileges only to the user.
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+This is another project on database management. It includes privileges, joins and other control managements.
