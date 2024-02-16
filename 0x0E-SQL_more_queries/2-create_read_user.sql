8b4538243a2b284076ebab8a316a83bc07488d1c
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-02-16 22:31:02 +0100

    Solve task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,7 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user
+USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_02@localhost IDENTIFIED by 'user_0d_2_pwd';
 -- grant SELECT privileges only to the user.
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
