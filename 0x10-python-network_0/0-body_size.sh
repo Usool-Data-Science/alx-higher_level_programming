44aa163b071f02a752de1c18aeecfe253eeb4f80
UsoolDataScience <adeshinaibrahim10@gmail.com>
2024-03-27 08:01:16 -0700

    Update the solution

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Prints the size of the response body
 curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/p.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+chmod +x *.sh
+git add .
+git commit -m 'Update the solution'
+git push
